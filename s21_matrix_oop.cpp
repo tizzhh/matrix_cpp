5c512db111ddf76ddfe53f2846aac1c955fc8dca
Vadim Kuznetsov <darovadraste@gmail.com>
2023-10-07 22:46:36 +0300

    Add operator =.

--- s21_matrix_oop.cpp
+++ s21_matrix_oop.cpp
@@ -41,4 +41,43 @@ S21Matrix::S21Matrix(S21Matrix &&other)
   other.matrix_ = nullptr;
 }
 
+S21Matrix &S21Matrix::operator=(const S21Matrix &other) {
+  if (this != &other) {
+    for (int i = 0; i < this->rows_; ++i) {
+      delete[] this->matrix_[i];
+    }
+    delete[] this->matrix_;
+
+    this->rows_ = other.rows_;
+    this->cols_ = other.cols_;
+    this->matrix_ = new double *[this->rows_];
+    for (int i = 0; i < this->rows_; ++i) {
+      this->matrix_[i] = new double[this->cols_];
+      for (int j = 0; j < this->cols_; ++j) {
+        this->matrix_[i][i] = other.matrix_[i][j];
+      }
+    }
+  }
+  return *this;
+}
+
+S21Matrix &S21Matrix::operator=(S21Matrix &&other) {
+  if (this != &other) {
+    for (int i = 0; i < this->rows_; ++i) {
+      delete[] this->matrix_[i];
+    }
+    delete[] this->matrix_;
+
+    this->rows_ = other.rows_;
+    this->cols_ = other.cols_;
+    this->matrix_ = other.matrix_;
+
+    other.rows_ = 0;
+    other.cols_ = 0;
+    other.matrix_ = nullptr;
+  }
+
+  return *this;
+}
+
 void S21Matrix::SumMatrix(const S21Matrix &other) {}
